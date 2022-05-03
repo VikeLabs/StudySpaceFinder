fd91463b0aa2d1ce9b0a63d383ee2b16fe096cd0
Scott <skenning128@gmail.com>
2022-05-02 19:18:12 -0700

    half kinda working

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,44 @@ import { useState } from 'react';
 
 import {getDatabase, ref, child, get} from 'firebase/database'
 function App() {
+
+  const fetchData = async () => {
+    const date = "202205";
+    const courseListUrl = `https://courseup.vikelabs.ca/api/courses/${date}`;
+
+    const response = await fetch(courseListUrl)
+      .then((response) => {return response.json()})
+      .then(data =>{
+        data.forEach((element: any) => {
+          let code = element.code
+          let subject = element.subject
+          let query = `https://courseup.vikelabs.ca/api/sections/${date}?subject=${subject}&code=${code}`
+          console.log(query)
+        });
+    })
+  }
   
-  const handleClick = (()=>{
-    const database = getDatabase()
-    const dbref = ref(database);
+  const handleClick = ( async ()=>{
+    // fetch()
+    // const database = getDatabase()
+    // const dbref = ref(database);
   
-    get(child(dbref,`Bob Wright`)).then((snapshot) => {
-      if(snapshot.exists()){
-        console.log(snapshot.val())
-      } else {
-        console.log("No data available")
-      }
-    }).catch((error)=>{
-      console.log(error)
-    });
+    // get(child(dbref,`Bob Wright`)).then((snapshot) => {
+    //   if(snapshot.exists()){
+    //     console.log(snapshot.val())
+    //   } else {
+    //     console.log("No data available")
+    //   }
+    // }).catch((error)=>{
+    //   console.log(error)
+    // });
   })
   
   return (
     <div className="App">
       <header className="App-header">
         Study Space Finder
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={fetchData}>Click me</button>
       </header>
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return (
+        <div className="header">
+
+        </div>
+    )
+}
