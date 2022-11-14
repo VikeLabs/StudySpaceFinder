6d8d267368ee9a3aebd5056c37ae89986461f9cc
Scott <skenning128@gmail.com>
2022-11-14 01:16:07 -0800

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom"
 import './App.css';
+import Home from "./components/Home/index"
+import About from "./components/About/index"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="about" element={ <About/> } />
+      </Routes>
     </div>
   );
 }
