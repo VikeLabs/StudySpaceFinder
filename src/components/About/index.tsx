0f2dc93c25e20f28377149ab1ddbe7a8e1d3f8ad
liamsquires <liam.squires@shaw.ca>
2022-12-09 12:19:18 -0800

    Started with the basics, unsure about file structure

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './css/about.module.css';
 
 function About() {
     return (
-        <div>
-            <h1>This is the about page</h1>
+        <div className={styles.div}>
+            <h1 className={styles.h1}>This is the about page</h1>
             <Link to="/">Click to view our home page</Link>
         </div>
     )
