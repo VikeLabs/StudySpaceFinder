e89cc35dccb032cabf8f2448f126fb62d730bb2d
HalN <haln_01@proton.me>
2023-01-31 16:41:16 -0800

    chore: types for home page / home page components

--- src/components/Home/BuildingCard/index.tsx
+++ src/components/Home/BuildingCard/index.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
-import style from "./BuildingCard.module.css"
+import style from "./BuildingCard.module.css";
 
-function BuildingCard(props: any) {
-    return (
-        <Link className={style.buildingCard} to={`classrooms?building=${props.building}`}>{props.building}</Link>
-    );
-  }
-  
-  export default BuildingCard;
+interface PropType {
+  building: string; // building name
+}
+
+function BuildingCard({ building }: PropType) {
+  return (
+    <Link className={style.buildingCard} to={`classrooms?building=${building}`}>
+      {building}
+    </Link>
+  );
+}
+
+export default BuildingCard;
---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export enum Day {
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+}
+
+export interface Time {
+  time_display: string;
+  time_start: number;
+  time_end: number;
+}
+
+export interface RoomData {
+  time: Time;
+  course: string;
+}
+
+export interface BuildingData {
+  // building: { room: { day: RoomData[] } }
+  [key: string]: {
+    [key: string]: {
+      [key in keyof typeof Day]: RoomData[];
+    };
+  };
+}
+
+export interface BuildingTime {
+  // building: { room: { day: boolean[] } }
+  [key: string]: {
+    [key: string]: {
+      [key in keyof typeof Day]: boolean[];
+    };
+  };
+}
