33386ffe2947ea7ab3c21de1740eb865156e89b9
Hal <haln_01@proton.me>
2023-02-03 10:53:10 -0800

    feat: CORS and serving building times

--- server/main.py
+++ server/main.py
@@ -1,17 +1,22 @@
 import json
-
+from urllib import parse
 from typing import Union
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["http://localhost:3000"], allow_methods=["GET"]
+)
 
 
 # ie: /Cornett%20Building/A120 - don't forget to encode whitespace for the url
 @app.get("/{bldg}/{room}", status_code=200)
 def get_room_info(bldg: str, room: str):
-    bldg = bldg.replace("%20", " ")
+    bldg = parse.unquote(bldg)
 
-    file_path = "./data/building_data.json"
+    file_path = "./data/building_time_intervals_0.1.json"
 
     # read json
     uvic_bldg = open(file_path)
@@ -24,3 +29,19 @@ def get_room_info(bldg: str, room: str):
         raise HTTPException(status_code=404)
 
     return building
+
+
+# ie: /Cornett%20Building
+@app.get("/{bldg}", status_code=200)
+def get_building_info(bldg: str):
+    b = parse.unquote(bldg)
+    file_path = "./data/building_time_intervals_0.1.json"
+
+    with open(file_path) as f:
+        data = json.load(f)
+        building = data.get(b)
+
+        if not building:
+            print("[{}] not found".format(b))
+            raise HTTPException(status_code=404)
+        return building
