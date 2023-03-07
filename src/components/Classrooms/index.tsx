aa1aacf7cc0404990bddc5ec7ff1cd40439a2ef7
Scott <skenning128@gmail.com>
2023-03-06 21:01:48 -0800

    remove unused code

--- src/components/Classrooms/index.tsx
+++ src/components/Classrooms/index.tsx
@@ -12,13 +12,6 @@ import { dateOptions } from "consts.js";
 import styles from "./Classrooms.module.css";
 import { useFetch } from "hooks/useFetch";
 
-
-interface fetchResponseType {
-  data: unknown;
-  loading: Boolean;
-  error: string;
-};
-
 function ClassroomCardsContainer(input: any) {
   const getCurrentDate = () => {
     const date = new Date();
@@ -33,13 +26,6 @@ function ClassroomCardsContainer(input: any) {
   const [building, setBuilding] = useState(params.get("building"));
   const [data, loading, error] = useFetch<any>(`/building/${building}?hour=${time.split(":")[0]}&minute=${time.split(":")[1]}&day=${day}`);
 
-  console.log(data)
-
-  const handleDayChange = (event: any) => {
-    console.log(event.target.value);
-    setDay(event.target.value);
-  };
-
   return loading ? (
     <p>Loading...</p>
   ) : error ? <p>{error}</p> : (
@@ -51,7 +37,7 @@ function ClassroomCardsContainer(input: any) {
           label="Day"
           value={day}
           options={dateOptions}
-          onChange={handleDayChange}
+          onChange={(e: any) => setDay(e.target.value)}
         />
       </div>
       <div className={styles.ClassroomCardsContainer}>
