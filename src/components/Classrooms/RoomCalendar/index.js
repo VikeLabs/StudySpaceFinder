96bc9b205fcec5ffd90904f3bbb729f07640126e
liamsquires <liam.squires@shaw.ca>
2023-03-15 11:46:54 -0700

    Adds events calendar -time is shifted

--- src/components/Classrooms/RoomCalendar/index.js
+++ src/components/Classrooms/RoomCalendar/index.js
@@ -27,12 +27,28 @@ function RoomCalendar (props){
         daysIncluded.forEach(day => {
             data.schedules[day].forEach(scheduledClass => {
                 console.log(scheduledClass);
-                //calendarEvents.push(createCalendarEvent)
+                calendarEvents.push(createCalendarEvent(scheduledClass.time_start, scheduledClass.time_end, scheduledClass.subject, day))
             })
         })
     }
 
+    function createCalendarEvent (startTime, endTime, name, day) {
+        let calendarEvent = {
+            text: name,
+            start: formatTime(startTime, day),
+            end: formatTime(endTime, day),
+            barColor: "#6aa84f",
+            id: DayPilot.guid()
+        }
+        return calendarEvent;
+    }
     
+    function formatTime (time, day) {
+        const dayToDateMap = new Map([["Monday","January 9, 2023 "],["Tuesday","January 10, 2023 "],["Wednesday","January 11, 2023 "],["Thursday","January 12, 2023 "],["Friday","January 13, 2023 "],["Saturday","January 14, 2023 "],["Sunday","January 15, 2023 "]])
+        let dayString = dayToDateMap.get(day);
+        let ftime = new Date(dayString + time);
+        return ftime;
+    }
     // if(data) {
     //     data.schedules.forEach(day => {
     //     data.schedules[day].forEach(event => {
@@ -45,25 +61,27 @@ function RoomCalendar (props){
     //Loop through monday until we get a false answer, then create event over that time range.
     //then loop through until we get a true, create event.
     //do it for every day, filling events
-/*
+
     //function to help string organization for registering calendar events - Got some issues, would need to be updated every semester right now
-    function getTimeByIndex (index, day) {
-        const dayToDateMap = new Map([["Monday","2023-01-09T"],["Tuesday","2023-01-10T"],["Wednesday","2023-01-11T"],["Thursday","2023-01-12T"],["Friday","2023-01-13T"]])
-        let dayString = dayToDateMap.get(day);
-        let hour = 8;
-        let minute = 0;
+    // function getTimeByIndex (index, day) {
+    //     const dayToDateMap = new Map([["Monday","2023-01-09T"],["Tuesday","2023-01-10T"],["Wednesday","2023-01-11T"],["Thursday","2023-01-12T"],["Friday","2023-01-13T"]])
+    //     let dayString = dayToDateMap.get(day);
+    //     let hour = 8;
+    //     let minute = 0;
 
-        hour = Math.floor(hour + index/2);
-        minute = (index*30)%60
-        if(minute === 0) {
-            minute = "00";
-        }
-        if(hour < 10) {
-            hour = "0" + hour;
-        }
-        return dayString + hour + ":" + minute + ":" + "00";
-    }
+    //     hour = Math.floor(hour + index/2);
+    //     minute = (index*30)%60
+    //     if(minute === 0) {
+    //         minute = "00";
+    //     }
+    //     if(hour < 10) {
+    //         hour = "0" + hour;
+    //     }
+    //     return dayString + hour + ":" + minute + ":" + "00";
+    // }
 
+    
+/*
     if (Object.keys(data).length !== 0) {  //This checks whether a classroom has no data, and although I haven't found a class like this, it's necesarry
         
         let daysIncluded = Object.keys(data.times);
@@ -162,7 +180,6 @@ function RoomCalendar (props){
         ) : error ? <p>{error}</p> : (
         <Container>
             <PageTitle name={data ? data.building + " " + data.room : ""}/>
-            <p>{data ? data.schedules.Friday[0].section : " "}</p>
             <DayPilotCalendar
                 {...calendarSettings}
             />
