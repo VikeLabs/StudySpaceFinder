0cdf18af0a07667b56426ed11c1341b03c942aca
Hal <haln_01@proton.me>
2023-03-16 10:08:54 -0700

    chore: removed frontend code

--- src/components/common/Header/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-    return (
-        <header>
-            Header
-        </header>
-    )
-}
--- src/util/getCurrentDay.ts
+++
@@ -1,3 +0,0 @@
-export const getCurrentDay = () => {
-  return new Date().toLocaleDateString('en-ca', { weekday: 'long' });
-}
