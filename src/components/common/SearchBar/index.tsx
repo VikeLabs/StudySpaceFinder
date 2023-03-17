511747db2375937428610eda06fe468cc31c9f5b
Scott <skenning128@gmail.com>
2023-03-17 09:26:24 -0700

    magnifying glass

--- src/components/common/SearchBar/index.tsx
+++ src/components/common/SearchBar/index.tsx
@@ -1,15 +1,16 @@
 import styles from "./SearchBar.module.css";
+import icon from "../../../assets/magnifying-glass-svgrepo-com.svg";
 
 
 const SearchBar = (props: any) => {
     return (
-      <input 
-       key="search-bar"
-       value={props.keyword}
-       placeholder={"Search buildings"}
-       onChange={(e) => props.onChange(e.target.value)}
-       className={styles.searchBar}
-      />
+        <input 
+         key="search-bar"
+         value={props.keyword}
+         placeholder={"Search buildings"}
+         onChange={(e) => props.onChange(e.target.value)}
+         className={styles.searchBar}
+        />
     );
   }
   
