e386e7ce87c34fcb728b54ef5e395456c5626a16
liamsquires <liam.squires@shaw.ca>
2023-03-28 11:19:04 -0700

    Styling + legend

--- src/components/Classrooms/RoomCalendar/index.js
+++ src/components/Classrooms/RoomCalendar/index.js
@@ -4,7 +4,7 @@ import { useSearchParams } from "react-router-dom";
 import Container from 'components/common/Container';
 import { PageTitle } from 'components/common/PageTitle';
 import { useFetch } from "hooks/useFetch";
-
+import './calendar.css'
 
 function RoomCalendar (props){
 
@@ -22,7 +22,6 @@ function RoomCalendar (props){
     //for each element of each key's array
     //add event to the calendar for that day
     if(data) {
-        console.log("DATA IS REAL");
         let daysIncluded = Object.keys(data.schedules);
         daysIncluded.forEach(day => {
             data.schedules[day].forEach(scheduledClass => {
@@ -46,7 +45,11 @@ function RoomCalendar (props){
     function formatTime (time, day) {
         const dayToDateMap = new Map([["Monday","January 9, 2023 "],["Tuesday","January 10, 2023 "],["Wednesday","January 11, 2023 "],["Thursday","January 12, 2023 "],["Friday","January 13, 2023 "],["Saturday","January 14, 2023 "],["Sunday","January 15, 2023 "]])
         let dayString = dayToDateMap.get(day);
-        let ftime = new Date(dayString + time + " UTC");
+        let ftime = new Date(dayString + time + " UTC"); 
+        //NOTE: the calendar shifts the time if it's not given in UTC, so I just stuck it on the end
+        //This is potentially gimmicky, the calendar might not be showing the actual timezone
+        //Shouldn't be an issue unless we want to display to other timezones, 
+        //or maybe daylight savings could throw it off
         return ftime;
     }
 
@@ -162,10 +165,14 @@ function RoomCalendar (props){
         viewType: "WorkWeek",
         headerDateFormat: "dddd",
         events: calendarEvents,
-        startDate: "2023-01-09",    //would need to be updated every semester
+        startDate: "2023-01-09",    
+        //The calendar alawys displays Jan 9th to Jan 13th, 2023
+        //but as long as the data passed in is for the right semester, 
+        //it shouldn't cause any problems.
         eventMoveHandling: "Disabled",
-        dayBeginsHour: "8",
+        dayBeginsHour: "8", //this apparently requires pro version
         scrollPositionHour: "8",
+        theme: "calendar"
     };
 
 
@@ -174,9 +181,15 @@ function RoomCalendar (props){
         ) : error ? <p>{error}</p> : (
         <Container>
             <PageTitle name={data ? data.building + " " + data.room : ""}/>
-            <DayPilotCalendar
-                {...calendarSettings}
-            />
+            <div className="calendarContainer">
+                <DayPilotCalendar
+                    {...calendarSettings}
+                />
+            </div>
+            <div className='legend'>
+                <div className='unavailableColour'></div>
+                <p className='legendText'>Unavailable</p>
+            </div>
         </Container>
     );
     
@@ -186,3 +199,4 @@ export default RoomCalendar
 
 //TODO:
 //Style the calendar (there is an online theme maker?)
+//Make a single day-column for mobile?
