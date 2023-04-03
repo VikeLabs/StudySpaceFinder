93d71571542cc118797b9874312f777947248028
Hal <haln_01@proton.me>
2023-04-03 11:42:50 -0700

    chore: api env

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-export const API = "http://127.0.0.1:8000/api";
-
 export const dateOptions = [
   { label: "Monday", value: 1 },
   { label: "Tuesday", value: 2 },
@@ -10,11 +8,25 @@ export const dateOptions = [
 
 export const vikeLabsText = `
   text
-`
+`;
 
 export const aboutText = `
   StudySpaceFinder is a Vikelabs project that displays which classrooms are empty 
   on campus. We use data from UVic's course registration system to find when classes 
   are booked. Note that this is not a source of truth, as there are things such as 
   guest seminars or UVic clubs that book rooms that aren't shown here.
-`
+`;
+
+// API RELATED
+export const API =
+  process.env.NODE_ENV === "production"
+    ? process.env.API
+    : "http://127.0.0.1:8000";
+
+if (API === "") throw new Error(`Missing API env variable on prod`);
+
+export const ENDPOINTS = {
+  getAllBuildings: `${API}/api/buildings/all`,
+  getBuilding: `${API}/api/buildings`,
+  getRoom: `${API}/api/rooms`,
+};
