f9b623d8e39a2e83af656d3350a0e20da2e0b312
Hal <haln_01@proton.me>
2023-04-03 12:26:13 -0700

    chore: connected classroom page api

--- src/components/Classrooms/index.tsx
+++ src/components/Classrooms/index.tsx
@@ -1,18 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ClassroomCard from "./ClassroomCard";
 import { useSearchParams } from "react-router-dom";
-import { mockFetch } from "mock";
 import Container from "components/common/Container";
 import { PageTitle } from "components/common/PageTitle";
-import times from "mock/building_time_intervals_0.1.json";
 import Dropdown from "components/common/Dropdown";
-import { getCurrentTimeIndex } from "util/getCurrentTimeIndex";
-import { getCurrentDay } from "util/getCurrentDay";
-import { dateOptions } from "consts";
+import { dateOptions, ENDPOINTS } from "consts";
 import styles from "./Classrooms.module.css";
 import { useFetch } from "hooks/useFetch";
 
-function ClassroomCardsContainer(input: any) {
+function ClassroomCardsContainer() {
   const getCurrentTime = () => {
     const date = new Date();
     const hours = date.getHours();
@@ -23,10 +19,11 @@ function ClassroomCardsContainer(input: any) {
 
   const [time, setTime] = useState(getCurrentTime());
   const [day, setDay] = useState(new Date().getDay());
-  const [params, setparams] = useSearchParams();
-  const [building, setBuilding] = useState(params.get("building"));
+  const [params] = useSearchParams();
+
+  const buildingId = params.get("building");
   const [data, loading, error] = useFetch<any>(
-    `/building/${building}?hour=${time.split(":")[0]}&minute=${
+    `${ENDPOINTS.getBuilding}/${buildingId}?hour=${time.split(":")[0]}&minute=${
       time.split(":")[1]
     }&day=${day}`
   );
