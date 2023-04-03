6878a4bbbabe608d8e904d7d8386d64711532916
Hal <haln_01@proton.me>
2023-04-03 12:52:22 -0700

    refactor: added types

--- src/types.ts
+++ src/types.ts
@@ -6,40 +6,19 @@ export enum Day {
   "Friday",
 }
 
-// TODO: delete
-export interface Time {
-  time_display: string;
-  time_start: number;
-  time_end: number;
-}
-
-// TODO: delete
-export interface RoomData {
-  time: Time;
-  course: string;
-}
-
-// TODO: delete
-export interface BuildingData {
-  // building: { room: { day: RoomData[] } }
-  [key: string]: {
-    [key: string]: {
-      [key in keyof typeof Day]: RoomData[];
-    };
-  };
-}
-
-// TODO: delete
-export interface BuildingTime {
-  // building: { room: { day: boolean[] } }
-  [key: string]: {
-    [key: string]: {
-      [key in keyof typeof Day]: boolean[];
-    };
-  };
-}
-
 export interface Building {
   id: number;
   name: string;
+}
+
+export interface Classroom {
+  room_id: number;
+  room: string;
+  next_class?: string;
+  subject?: string;
+}
+
+export interface ClassroomSummary {
+  building: string;
+  data: Classroom[];
 }
