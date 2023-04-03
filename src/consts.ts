a829a0ce3826be19ba1572010f08ee786c2a71ea
Hal <haln_01@proton.me>
2023-04-03 13:33:19 -0700

    feat: about page - css changes - contributors - source code

--- src/consts.ts
+++ src/consts.ts
@@ -30,3 +30,19 @@ export const ENDPOINTS = {
   getBuilding: `${API}/api/buildings`,
   getRoom: `${API}/api/rooms`,
 };
+
+// ABOUT RELATED
+export const DISCLAIMER =
+  "Study Space Finder is an interface developed by students of University of Victoria (UVic).\nPlease note that the information provided on this platform is based on the schedules found in UVic officials. However, we remain independent and there may be certain information that we do not have access to. Therefore, we cannot guarantee the accuracy, completeness, or reliability of the information provided. We strongly recommend that users verify any information obtained from this platform with official sources before making any decisions or taking any actions based on such information. By using this platform, you acknowledge and agree that you assume full responsibility for any decisions or actions taken based on the information provided.\nEnjoy.";
+
+export const CONTRIBUTORS = [
+  { name: "Scott", github: "https://github.com/Scott-Kenning" },
+  { name: "Liam", github: "https://github.com/liamsquires" },
+  { name: "Devin", github: "https://github.com/DevinFrioud" },
+  { name: "Hal", github: "https://github.com/hn275" },
+];
+
+export enum SOURCE {
+  client = "https://github.com/Scott-Kenning/StudySpaceFinder",
+  api = "https://github.com/hn275/StudySpaceFinder",
+}
