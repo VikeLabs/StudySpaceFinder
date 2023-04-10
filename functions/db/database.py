c0ce9db4d254d8ec7160e82c3baeda81ed264ad2
Hal <haln_01@proton.me>
2023-04-10 12:30:02 -0700

    feat: fetching script

--- functions/db/database.py
+++ functions/db/database.py
@@ -4,7 +4,7 @@ import sqlite3 as sql
 
 class Database:
     def __enter__(self):
-        self.connection = sql.connect(os.path.join("functions", "db", ".database.db"))
+        self.connection = sql.connect(os.path.join("db", ".database.db"))
         self.cursor = self.connection.cursor()
         return self
 
