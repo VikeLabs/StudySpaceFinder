47ae219735f00a7704fa2e220a37880842dc3476
Scott <skenning128@gmail.com>
2023-06-19 18:15:10 -0700

    check for about

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,7 +1,7 @@
 describe('My First Test', () => {
     it('Visits the app', () => {
       cy.visit('http://localhost:3000')
-      cy.get('nav').contains('Study Space Finder');
+      cy.get('nav').contains('About');
     })
   })
   
