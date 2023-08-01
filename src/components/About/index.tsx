7c26ec5d43cfa427b95df33ee24404ec0bbb8db4
Scott-Kenning <78236293+Scott-Kenning@users.noreply.github.com>
2023-07-31 21:16:53 -0700

    small fixes (#36)
    * small fixes
    
    * PR Fixes

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./css/about.module.css";
+import styles from "./about.module.css";
 import Container from "components/common/Container";
 import { CONTRIBUTORS, DISCLAIMER, SOURCE } from "consts";
 import shortid from "shortid";
@@ -10,73 +10,75 @@ import classNames from "classnames";
 function About() {
   return (
     <Container>
-      <section className={classNames(styles.section, styles.firstSection)}>
-        <h2>About</h2>
-        <p className={styles.p}>
-          Study Space Finder makes it easy to find study spaces at the University
-          of Victoria!
-        </p>
-        <p className={styles.p}>
-          You can use the link <Link to="/" className={styles.link}>here</Link> to find rooms that
-          aren't currently in use.
-        </p>
-        <p className={styles.p}>
-          Right now, our data only considers UVic classes. Rooms may be booked
-          for other that we aren't aware of such as club meetings or guest lectures.
-        </p>
-      </section>
-
-      <section className={styles.section}>
-        <h2>VikeLabs</h2>
-        <p className={styles.p}>
-          Study Space Finder was developed by students at{" "}
-          <a href="https://www.vikelabs.ca/" className={styles.link}>VikeLabs</a>, a software
-          development club at UVic.
-        </p>
-        <p className={styles.p}>
-          <Anchor href="https://github.com/Scott-Kenning/StudySpaceFinder" className={styles.link}>
-            See our GitHub repository
-          </Anchor>
-        </p>
-      </section>
-      {/* We could include a suggestions section. For example, CourseUp links to github discussions and a google form for feedback. */}
-
-      <section className={styles.section}>
-        <h2>Disclaimer</h2>
-        {/* {DISCLAIMER.split("\n").map((p) => (
-          <p key={shortid.generate()} className={styles.p}>
-            {p}
+      <div className={styles.innerContainer}>
+        <section className={classNames(styles.section, styles.firstSection)}>
+          <h2>About</h2>
+          <p className={styles.p}>
+            Study Space Finder makes it easy to find study spaces at the University
+            of Victoria!
           </p>
-        ))} */}
-        <p>
-        Study Space Finder is an interface developed by students of University of Victoria (UVic). 
-        </p>
-        <p>
-          Please note that the information provided on this platform is based on the information found in UVic websites. However, we remain independent and there may be certain information that we do not have access to. Therefore, we cannot guarantee the accuracy, completeness, or reliability of the information provided. We strongly recommend that users verify any information obtained from this website with official sources before making any decisions or taking any actions based on such information.
-        </p>
-      </section>
+          <p className={styles.p}>
+            You can use the link <Link to="/" className={styles.link}>here</Link> to find rooms that
+            aren't currently in use.
+          </p>
+          <p className={styles.p}>
+            Right now, our data only considers UVic classes. Rooms may be booked
+            for other that we aren't aware of such as club meetings or guest lectures.
+          </p>
+        </section>
 
-      <section className={styles.section}>
-        <h2>Contributors</h2>
-        {CONTRIBUTORS.map(({ name, github }) => (
-          <Anchor href={github} aria-label="github" key={shortid.generate()}>
-            <FaGithub />
-            &nbsp;
-            <span>{name}</span>
-          </Anchor>
-        ))}
-      </section>
+        <section className={styles.section}>
+          <h2>VikeLabs</h2>
+          <p className={styles.p}>
+            Study Space Finder was developed by students at{" "}
+            <a href="https://www.vikelabs.ca/" className={styles.link}>VikeLabs</a>, a software
+            development club at UVic.
+          </p>
+          <p className={styles.p}>
+            <Anchor href="https://github.com/Scott-Kenning/StudySpaceFinder" className={styles.link}>
+              See our GitHub repository
+            </Anchor>
+          </p>
+        </section>
+        {/* We could include a suggestions section. For example, CourseUp links to github discussions and a google form for feedback. */}
 
-      <section className={styles.section}>
-        <h3>
-          Study Space Finder is&nbsp;
-          <Anchor href={SOURCE.client} className={styles.link}>open source</Anchor>!
-        </h3>
-        <p>
-          If you are interested in consuming the API, read the&nbsp;
-          <Anchor href={SOURCE.api} className={styles.link}>docs</Anchor>.
-        </p>
-      </section>
+        <section className={styles.section}>
+          <h2>Disclaimer</h2>
+          {/* {DISCLAIMER.split("\n").map((p) => (
+            <p key={shortid.generate()} className={styles.p}>
+            {p}
+            </p>
+          ))} */}
+          <p>
+          Study Space Finder is an interface developed by students of University of Victoria (UVic). 
+          </p>
+          <p>
+            Please note that the information provided on this platform is based on the information found in UVic websites. However, we remain independent and there may be certain information that we do not have access to. Therefore, we cannot guarantee the accuracy, completeness, or reliability of the information provided. We strongly recommend that users verify any information obtained from this website with official sources before making any decisions or taking any actions based on such information.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Contributors</h2>
+          {CONTRIBUTORS.map(({ name, github }) => (
+            <Anchor href={github} aria-label="github" key={shortid.generate()} className={styles.githubCard}>
+              <FaGithub />
+              &nbsp;
+              <span>{name}</span>
+            </Anchor>
+          ))}
+        </section>
+
+        <section className={styles.section}>
+          <h3>
+            Study Space Finder is&nbsp;
+            <Anchor href={SOURCE.client} className={styles.link}>open source</Anchor>!
+          </h3>
+          <p>
+            If you are interested in consuming the API, read the&nbsp;
+            <Anchor href={SOURCE.api} className={styles.link}>docs</Anchor>.
+          </p>
+        </section>
+      </div>
     </Container>
   );
 }
---
+++ src/components/Classrooms/RoomCalendar/index.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
+import { useSearchParams } from "react-router-dom";
+import Container from 'components/common/Container';
+import { PageTitle } from 'components/common/PageTitle';
+import { useFetch } from "hooks/useFetch";
+import { API } from "consts";
+import './calendar.css'
+
+interface ScheduledClass {
+  time_start: string;
+  time_end: string;
+  subject: string;
+}
+
+interface CalendarData {
+  [key: string]: ScheduledClass[] | undefined;
+}
+
+const RoomCalendar: React.FC = () => {
+    const params = new URLSearchParams(window.location.search);
+    const [room, setRoom] = useState<string | null>(params.get('room'))
+    const [roomId, setRoomId] = useState<string | null>(params.get('roomid'))
+    const [building, setBuilding] = useState<string | null>(params.get('building'))
+    const url = `${API.getRoom}/${roomId}`;
+    const [data, loading, error] = useFetch<CalendarData | null>(url);
+
+    let calendarEvents: any[] = [];
+    if (data) {
+        let daysIncluded = Object.keys(data);
+        daysIncluded.forEach(day => {
+            data[day]?.forEach((scheduledClass: ScheduledClass) => {
+                calendarEvents.push(createCalendarEvent(scheduledClass.time_start, scheduledClass.time_end, scheduledClass.subject, day));
+            });
+        });
+    }
+
+    function createCalendarEvent(startTime: string, endTime: string, name: string, day: string): any {
+        let calendarEvent: any = {
+            text: name,
+            start: formatTime(startTime, day),
+            end: formatTime(endTime, day),
+            barColor: "#6aa84f",
+            id: DayPilot.guid()
+        }
+        return calendarEvent;
+    }
+
+    function formatTime(time: string, day: string): Date {
+        const dayToDateMap = new Map([["Monday","January 9, 2023 "],["Tuesday","January 10, 2023 "],["Wednesday","January 11, 2023 "],["Thursday","January 12, 2023 "],["Friday","January 13, 2023 "],["Saturday","January 14, 2023 "],["Sunday","January 15, 2023 "]])
+        let dayString = dayToDateMap.get(day);
+        let ftime = new Date(dayString + time + " UTC"); 
+        return ftime;
+    }
+
+    let calendarSettings: any = {
+        viewType: "WorkWeek",
+        headerDateFormat: "dddd",
+        events: calendarEvents,
+        startDate: "2023-01-09",
+        eventMoveHandling: "Disabled",
+        eventResizeHandling: "Disabled",
+        eventEditHandling: "Disabled",
+        timeRangeSelectedHandling: "Disabled",
+        dayBeginsHour: 8,
+        scrollPositionHour: 8,
+        theme: "calendar_default"
+    };
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>{error}</p>
+    return (
+        <Container>
+            <PageTitle name={data ? building + " " + room  + " Weekly Schedule" : ""}/>
+            <div className="calendarContainer">
+                <DayPilotCalendar
+                    {...calendarSettings}
+                />
+            </div>
+            <div className='legend'>
+                <div className='unavailableColour'></div>
+                <p className='legendText'>Unavailable</p>
+            </div>
+        </Container>
+    );
+}
+
+export default RoomCalendar;
