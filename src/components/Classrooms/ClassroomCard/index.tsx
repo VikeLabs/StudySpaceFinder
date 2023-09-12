1da37c8827acf957bc3fdf63605d055534cf8714
Scott-Kenning <78236293+Scott-Kenning@users.noreply.github.com>
2023-09-12 10:38:58 -0700

    Bug fixes + use current_class (#40)
    * temp
    
    * working again

--- src/components/Classrooms/ClassroomCard/index.tsx
+++ src/components/Classrooms/ClassroomCard/index.tsx
@@ -1,25 +1,31 @@
 import { Classroom } from "types";
 import styles from "./classrooms.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 interface PropType {
-  room_id: number
-  room: string
-  next_class: string
-  subject: string
-  building: string; // building name
+  room_id: number;
+  room: string;
+  next_class: any;
+  subject: string;
+  building: string;
+  current_class: string;
 }
 
-function ClassroomCards(props : PropType) {
+function ClassroomCards(props: PropType) {
   return (
-    <Link className={styles.link} to={`calendar?roomid=${props.room_id}&building=${props.building}&room=${props.room}`}>
+    <Link
+      className={styles.link}
+      to={`calendar?roomid=${props.room_id}&building=${props.building}&room=${props.room}`}
+    >
       <section className={styles.cardContainer}>
         <h1 className={styles.cardTitle}>{props.room}</h1>
 
         <p className={styles.cardDesc}>
-          {props.next_class ? (
+          {props.current_class ? (
+            <>Currently busy</>
+          ) : props.next_class ? (
             <>
-              {props.subject} at {props.next_class}
+              Free until {props.next_class.time_start}
             </>
           ) : (
             <>Free until end of day</>
