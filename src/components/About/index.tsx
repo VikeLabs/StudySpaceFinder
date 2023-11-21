7a48d19f5794fe1f377babd9e2d0fe05fc672571
Scott-Kenning <78236293+Scott-Kenning@users.noreply.github.com>
2023-11-20 21:58:12 -0800

    About page changes (#45)
    * fix: include free til eod class room
    
    * fix: add 0 padding for minutes format
    
    * chore: refetching data
    
    * fix: removed hard coded end time for eod
    - leave it to frontend.
    
    * fix: with syntax
    - close db connection if exception is raised
    
    * fix: sort buildings by name
    
    * Update README.md
    
    * add comment
    
    * fix bugs from new data structure, add loading modal, display special characters properly
    
    * style fixes
    
    ---------
    
    Co-authored-by: HalN <haln_01@proton.me>

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./about.module.css";
 import Container from "components/common/Container";
-import { CONTRIBUTORS, DISCLAIMER, SOURCE } from "consts";
+import { CONTRIBUTORS } from "consts";
 import shortid from "shortid";
 import { FaGithub } from "react-icons/fa";
 import { Anchor } from "components/common/Anchor";
@@ -9,7 +9,7 @@ import classNames from "classnames";
 
 function About() {
   return (
-    <Container>
+    <Container bottomPadding={false}>
       <div className={styles.innerContainer}>
         <section className={classNames(styles.section, styles.firstSection)}>
           <h2>About</h2>
@@ -17,13 +17,17 @@ function About() {
             Study Space Finder makes it easy to find study spaces at the University
             of Victoria!
           </p>
+          <br/>
           <p className={styles.p}>
-            You can use the link <Link to="/" className={styles.link}>here</Link> to find rooms that
-            aren't currently in use.
+            Study Space Finder is a Vikelabs project that shows where empty classes are on campus.
+            You can check a building's classrooms to see which ones are currently not in use,
+            or click on an individual classroom to see its schedule for the week.
+            Study Space Finder is still in progress, so there may be the occasional bug or slow page load.
           </p>
+          <br/>
           <p className={styles.p}>
-            Right now, our data only considers UVic classes. Rooms may be booked
-            for other that we aren't aware of such as club meetings or guest lectures.
+            Please note that we only have access to the lecture schedules posted by UVic.
+            Sometimes lectures are moved, clubs have booked rooms, or other events are happening that we aren't aware of.
           </p>
         </section>
 
@@ -34,31 +38,18 @@ function About() {
             <a href="https://www.vikelabs.ca/" className={styles.link}>VikeLabs</a>, a software
             development club at UVic.
           </p>
+          <br/>
           <p className={styles.p}>
             <Anchor href="https://github.com/Scott-Kenning/StudySpaceFinder" className={styles.link}>
               See our GitHub repository
             </Anchor>
           </p>
         </section>
-        {/* We could include a suggestions section. For example, CourseUp links to github discussions and a google form for feedback. */}
-
-        <section className={styles.section}>
-          <h2>Disclaimer</h2>
-          {/* {DISCLAIMER.split("\n").map((p) => (
-            <p key={shortid.generate()} className={styles.p}>
-            {p}
-            </p>
-          ))} */}
-          <p>
-          Study Space Finder is an interface developed by students of University of Victoria (UVic). 
-          </p>
-          <p>
-            Please note that the information provided on this platform is based on the information found in UVic websites. However, we remain independent and there may be certain information that we do not have access to. Therefore, we cannot guarantee the accuracy, completeness, or reliability of the information provided. We strongly recommend that users verify any information obtained from this website with official sources before making any decisions or taking any actions based on such information.
-          </p>
-        </section>
 
         <section className={styles.section}>
           <h2>Contributors</h2>
+          <div className={styles.githubContainer}>
+
           {CONTRIBUTORS.map(({ name, github }) => (
             <Anchor href={github} aria-label="github" key={shortid.generate()} className={styles.githubCard}>
               <FaGithub />
@@ -66,17 +57,7 @@ function About() {
               <span>{name}</span>
             </Anchor>
           ))}
-        </section>
-
-        <section className={styles.section}>
-          <h3>
-            Study Space Finder is&nbsp;
-            <Anchor href={SOURCE.client} className={styles.link}>open source</Anchor>!
-          </h3>
-          <p>
-            If you are interested in consuming the API, read the&nbsp;
-            <Anchor href={SOURCE.api} className={styles.link}>docs</Anchor>.
-          </p>
+          </div>
         </section>
       </div>
     </Container>
